1fb93fcaadba6d0e7f61ae57a7fee6bfa16d4fef
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-23 19:33:39 +0800

    CherishOS: rova: Create cherish makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_rova-user \
     lineage_rova-userdebug \
     lineage_rova-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cherish_rova.mk
+
+COMMON_LUNCH_CHOICES += \
+    cherish_rova-user \
+    cherish_rova-userdebug \
+    cherish_rova-eng
