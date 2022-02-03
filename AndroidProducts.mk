dc266eed970cbcbd0a719f2743d4aba39fd7f39c
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-02-03 15:18:20 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,12 +5,12 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rova.mk
+    $(LOCAL_DIR)/octavi_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rova-user \
-    lineage_rova-userdebug \
-    lineage_rova-eng
+    octavi_rova-user \
+    octavi_rova-userdebug \
+    octavi_rova-eng
 
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/cherish_rova.mk
